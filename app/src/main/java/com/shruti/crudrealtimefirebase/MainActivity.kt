5f62ec12c066c3655c6fa3348c5aa092b8c7bcbb
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-02 20:17:47 +0530

    crud

--- app/src/main/java/com/shruti/crudrealtimefirebase/MainActivity.kt
+++ app/src/main/java/com/shruti/crudrealtimefirebase/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.icu.text.CaseMap.Title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.shruti.crudrealtimefirebase.databinding.ActivityMainBinding
 import com.shruti.crudrealtimefirebase.databinding.CustomDialogBinding
@@ -43,21 +44,44 @@ class MainActivity : AppCompatActivity(),NotesInterface {
                         description = dialogBinding.etdescription.text.toString()
                     )
                 )
+
                 dialog.dismiss()
             }
-
         }
         dialog.show()
     }
-    fun save(){
-
-    }
 
     override fun updateNotes(dataClass: NotesDataClass, position: Int) {
+        var dialog = Dialog(this)
+        var dialogBinding = CustomDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialogBinding.title = "Title"
+        dialogBinding.description = "Description"
+        dialogBinding.save = "Save"
+        dialogBinding.ettitle.setText(dataClass.title)
+        dialogBinding.etdescription.setText(dataClass.description)
+        dialogBinding.btnsave.setOnClickListener {
+            if (dialogBinding.ettitle.text.isNullOrEmpty()){
+                dialogBinding.ettitle.error ="Enter title"
+            }
+            else if (dialogBinding.etdescription.text.isNullOrEmpty()){
+                dialogBinding.etdescription.error ="Enter description"
+            }
+            else
+            {
+//                item.set(position, NotesDataClass(title = dialogBinding.ettitle.text.toString(),
+//                description = dialogBinding.etdescription.text.toString())
+//                )
+
+                dialog.dismiss()
+            }
+            adapter.notifyDataSetChanged()
+        }
+        dialog.show()
 
     }
-
     override fun deleteNotes(dataClass: NotesDataClass, position: Int) {
-
+        item.removeAt(position)
+        adapter.notifyDataSetChanged()
     }
 }
